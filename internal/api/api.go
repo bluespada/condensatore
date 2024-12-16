4a97f0e991a40ea55e65644eff4630f49aef1924
bluespada <pentingmain@gmail.com>
2024-12-16 20:41:20 +0800

    feat: renew structure with golang as primary languages

---
+++ internal/api/api.go
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2024 Condensatore Contributor
+ * Copyright (C) 2024 Bluespada <pentingmain@gmail.com>
+ * 
+ * This file is part of Condensatore.
+ * 
+ * Condensatore is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ * 
+ * You should have received a copy of the MIT License along with Condensatore.
+ * If not, see <https://mit-license.org/>.
+ * 
+*/
+package api
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func RouteAttach(app *gin.Engine){
+    // Do create route from multiple api
+}
